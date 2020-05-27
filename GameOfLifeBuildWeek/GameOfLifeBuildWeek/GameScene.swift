409dc754521166415b4c037dc2f7642b88d94e21
Tyler Christian <tylerc71197@gmail.com>
2020-05-27 11:39:34 -0400

    Finished cells files next comes the viewController and storyboard

--- GameOfLifeBuildWeek/GameOfLifeBuildWeek/GameScene.swift
+++ GameOfLifeBuildWeek/GameOfLifeBuildWeek/GameScene.swift
@@ -11,7 +11,7 @@ import SpriteKit
 
 final class GameScene: SKScene {
     private var cellsView: SKView
-    private let cellspace :CGFloat = 1.0
+    private let cellSpace :CGFloat = 1.0
     private let cameraNode = SKCameraNode()
     private let viewModel: GameSceneModel
     static let backColor: UIColor = UIColor.black
@@ -22,6 +22,76 @@ final class GameScene: SKScene {
     }
     
     init(cellsView: SKView, viewModel: GameSceneModel) {
-        
+        self.viewModel = viewModel
+        self.cellsView = cellsView
+        super.init(size: cellsView.bounds.size)
+        addCells()
+        cellsView.presentScene(scene)
+        cellsView.preferredFramesPerSecond = 25
+        cellsView.isPaused = false
+        backgroundColor = GameScene.backColor
+        cameraNode.position = CGPoint(x: size.width / 2.0, y: size.height / 2.0)
+        addChild(cameraNode)
+        camera = cameraNode
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addCells() {
+        let columns = viewModel.columns
+        let cellSize = (cellsView.bounds.width - (cellSpace * CGFloat(columns - 1))) / CGFloat(columns)
+        let rows = Int((cellsView.bounds.height) / (cellSize + cellSpace))
+        viewModel.rows = rows
+        var cellCount = 0
+        
+        for row in 0..<columns {
+            for column in 0..<columns {
+                let frame = CGRect(x: CGFloat(column) * (cellSize + cellSpace),
+                                   y: CGFloat(row) * (cellSize + cellSpace),
+                                   width: cellSize,
+                                   height: cellSize)
+                let cellView = CellView(frame: frame)
+                let matrixArray: [Int] = [(cellCount - (columns + 1)),
+                                          (cellCount - columns),
+                                          (cellCount - (columns-1)),
+                                          (cellCount + 1),
+                                          (cellCount + (columns + 1)),
+                                          (cellCount + columns),
+                                          (cellCount + (columns-1)),
+                                          (cellCount - 1)]
+                let cellState = CellViewModel(age: 0, alive: false, alivePrePass: false, matrixArray: matrixArray, view: cellView)
+
+                viewModel.cellViewArray.append(cellState)
+                cellView.fillColor = GameScene.backColor
+
+                viewModel.addInnerCell(cellState: cellState, row: row, column: column)
+                addChild(cellView)
+                cellCount += 1
+            }
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        viewModel.cellViewInnerArray.forEach { viewModel.checkLifeState(cellState: $0) }
+        viewModel.cellViewInnerArray.forEach { $0.alive = $0.alivePrePass }
+        viewModel.generation += 1
+    }
+    
+    func newGame() {
+        viewModel.newGame()
+        cameraNode.run(SKAction.sequence([
+            SKAction.group([SKAction.fadeOut(withDuration: 0.25), SKAction.scale(to: 1.1, duration: 0.25)]),
+            SKAction.group([SKAction.scale(to: 1.0, duration: 0.25), SKAction.fadeIn(withDuration: 0.25)])]))
+        {
+            self.cellsView.isPaused = false
+        }
+    }
+    
+    func togglePaused() {
+        cellsView.isPaused.toggle()
+    }
+    
 }
+
--- GameOfLifeBuildWeek/GameOfLifeBuildWeek/Models/CellViewModel.swift
+++ GameOfLifeBuildWeek/GameOfLifeBuildWeek/Models/CellViewModel.swift
@@ -10,6 +10,29 @@ import Foundation
 import UIKit
 
 
-class CellViewModel {
-    
+class CellViewModel: CellViewDelegate {
+    var age: Int
+    var matrixArray: [Int]
+    var view: CellView
+    var alivePrePass: Bool
+    var alive: Bool {
+        didSet {
+            view.fillColor = alive ? UIColor(hue: (CGFloat(age) * 0.03), saturation: 1.0, brightness: 1.0, alpha: 1.0) : GameScene.deadColor
+        }
+    }
+
+    init(age: Int, alive: Bool, alivePrePass: Bool, matrixArray: [Int], view: CellView) {
+        self.age = age
+        self.alive = alive
+        self.alivePrePass = alivePrePass
+        self.matrixArray = matrixArray
+        self.view = view
+        self.view.delegate = self
+    }
+
+    // MARK: - CellViewDelegate
+
+    func cellViewDidToggleAlive(_ cellView: CellView) {
+        alive.toggle()
+    }
 }
