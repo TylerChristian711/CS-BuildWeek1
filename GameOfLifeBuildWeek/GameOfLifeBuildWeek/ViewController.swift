cd12b3542425fb7f4ad2862a84c0ce985b7de7a9
Tyler Christian <tylerc71197@gmail.com>
2020-05-27 14:54:51 -0400

    cells now display

--- GameOfLifeBuildWeek/GameOfLifeBuildWeek/ViewController.swift
+++ GameOfLifeBuildWeek/GameOfLifeBuildWeek/ViewController.swift
@@ -2,19 +2,39 @@
 //  ViewController.swift
 //  GameOfLifeBuildWeek
 //
-//  Created by Lambda_School_Loaner_218 on 5/26/20.
+//  Created by Lambda_School_Loaner_218 on 5/27/20.
 //  Copyright © 2020 Lambda_School_Loaner_218. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cellsView: SKView!
+    @IBOutlet weak var pauseButton: UIButton!
+    private var gameScene: GameScene?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gameScene = GameScene(cellsView: cellsView, viewModel: GameSceneModel(isIPad: UIDevice.current.userInterfaceIdiom == .pad))
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    @IBAction func newGameTapped(_ sender: Any) {
+        gameScene?.newGame()
+    }
+    
+    
+    @IBAction func pauseTapped(_ sender: Any) {
+        gameScene?.togglePaused()
+        pauseButton.setTitle(gameScene?.title, for: .normal)
+    }
+    
 
+    
 
 }
-
